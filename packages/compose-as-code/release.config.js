395b6c372cf65fb43c128d3414e9f8f5cd383ab0
flamestro <noreplyflamestro@gmail.com>
2023-06-20 22:18:41 +0200

    fix(cac): remove github plugin

--- packages/compose-as-code/release.config.js
+++ packages/compose-as-code/release.config.js
@@ -3,7 +3,6 @@ module.exports = {
     plugins: [
         '@semantic-release/commit-analyzer',
         '@semantic-release/release-notes-generator',
-        '@semantic-release/github',
         '@semantic-release/npm',
     ],
 };
