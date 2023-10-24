bb4446b0611d545d1795cf6df626534848ff4ece
flamestro <noreplyflamestro@gmail.com>
2023-10-24 15:24:32 +0200

    chore(cac): export compile function

--- packages/compose-as-code/src/index.ts
+++ packages/compose-as-code/src/index.ts
@@ -3,3 +3,4 @@ export { Composition, CompositionProps } from './composition/composition';
 export { Network } from './composition/network';
 export { Volume, VolumeProps } from './composition/volume';
 export { App } from './composition/app';
+export { compile } from './compiler/compositionCompiler'
