0b7ceb88be0d1f9d8be501a53b0420e95d8c10d1
flamestro <noreplyflamestro@gmail.com>
2024-05-25 12:52:24 +0200

    chore(CreCo): Update cac version + update workflow to publish packages

--- packages/create-composition/src/template/src/compositions/redisComposition.ts
+++ packages/create-composition/src/template/src/compositions/redisComposition.ts
@@ -11,7 +11,7 @@ export class RedisComposition extends Composition {
   constructor(scope: App, id: string, props: CompositionProps) {
     super(scope, id, props);
     const volume = new Volume(this, 'Volume', {});
-    const network = new Network(this, 'Network', {});
+    const network = new Network(this, 'Network');
     new RedisService(this, 'RedisService', { volume, network });
   }
 }
